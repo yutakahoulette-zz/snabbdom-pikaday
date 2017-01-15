b0f7e1c8ae70cdd75e3a3443ee3b6b3024d5875e
yutaka_houlette <yutakahoulette@gmail.com>
2017-01-14 17:52:15 -0800

    Use vanilla JS instead of ramda for merging

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import R from 'ramda'
 import h from 'snabbdom/h'
 import pik from 'pikaday'
 
+const merge = (obj1, obj2) => {
+  for (var attrname in obj2) { 
+    obj1[attrname] = obj2[attrname] 
+  }
+  return obj1
+}
+
 const bindPik = config => vnode => {
-  config = R.merge(config || {}, {field: vnode.elm})
+  config = merge(config || {}, {field: vnode.elm})
   new pik(config)
 }
 
 module.exports = (data, config) => {
-  data = R.merge(data || {}, {hook: {insert: bindPik(config)}})
+  data = merge(data || {}, {hook: {insert: bindPik(config)}})
   return h('input', data)
 }
 
